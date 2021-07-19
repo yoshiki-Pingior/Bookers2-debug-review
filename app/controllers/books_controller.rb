620b63017af08d8ff5b4f8de3d76021974ce0ecc
yoshiki0756 <g18lb014.yoshiki@gmail.com>
2021-07-19 09:48:24 +0000

    [Finish]Application Issues2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,10 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
 
   def show
     @book = Book.find(params[:id])
+    @book_new = Book.new
   end
 
   def index
@@ -35,16 +38,23 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
-    redirect_to books_path
+    @book.destroy
+    redirect_to books_path, notice: "successfully delete book!"
   end
 
   private
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+  
+  def ensure_correct_user
+   @book = Book.find(params[:id])
+   unless @book.user == current_user
+     redirect_to books_path
+   end
   end
 
 end
