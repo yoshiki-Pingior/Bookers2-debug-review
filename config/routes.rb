329e2858d7e4972fba6d0461e16a4214e8d041a7
yoshiki0756 <g18lb014.yoshiki@gmail.com>
2021-07-24 12:43:55 +0000

    [Add]relationships_function

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
   
   devise_for :users
-  resources :users,only: [:show,:index,:edit,:update]
+  
+  resources :users,only: [:show,:index,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
+  
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
