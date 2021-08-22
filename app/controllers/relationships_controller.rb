c4cf198d2a8efe74ea8ad872e333a029d0e85984
yoshiki0756 <g18lb014.yoshiki@gmail.com>
2021-08-22 12:18:28 +0000

    [modified]indent

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,20 +3,16 @@ class RelationshipsController < ApplicationController
     current_user.follow(params[:user_id])
     redirect_to request.referer
   end
-
   def destroy
     current_user.unfollow(params[:user_id])
     redirect_to request.referer  
   end
-  
   def followings
     user = User.find(params[:user_id])
     @users = user.followings
   end
-
   def followers
     user = User.find(params[:user_id])
     @users = user.followers
   end
-  
 end
